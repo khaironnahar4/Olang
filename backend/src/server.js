69fb89cba310ca236b06933bc1e6b1ed99c6e60d
khaironnahar4 <khaironnahar247@gmail.com>
2026-06-26 10:22:09 +0600

    correct environment variable casing for PORT and update BASE_URL for production

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,7 +11,7 @@ import path from 'path';
 dotenv.config();
 
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 const __dirname = path.resolve();
 
