7ce5d45130a495881146eabb324126f235fd6d62
khaironnahar4 <khaironnahar247@gmail.com>
2026-06-26 10:39:53 +0600

    update BASE_URL for production environment

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:8000/api" : "https://olang-1.onrender.com/api"
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:8000/api" : "/api"
 
 export const axiosInstance = axios.create({
     baseURL: BASE_URL,
